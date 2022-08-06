4ae1e4d8b3b47afc0f80c1862c1167c84caa5694
FabioJRMoreira <tulkas.fabio@hotmail.com>
2022-08-06 19:59:00 -0400

    ajustado model para lombok

--- src/main/java/com/fabio/fileUpload/model/FileDB.java
+++ src/main/java/com/fabio/fileUpload/model/FileDB.java
@@ -1,14 +1,16 @@
 package com.fabio.fileUpload.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="files")
-
+@Getter
+@Setter
 public class FileDB {
-
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name="uuid",strategy = "uuid2")
